a5123116a9341018f80efe53153c63e294d5284b
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2020-02-03 21:30:13 +0100

    Improve documentation

--- source/ezdb/package.d
+++ source/ezdb/package.d
@@ -1,9 +1,16 @@
 /**
 EzDb is a framework that allows one to easily interact with SQLite databases.
+
+Note that importing `ezdb` does not import all subpackages automatically.
+It only exports symbols interesting to users of the library, not ones that are
+only have used internally. These symbols can be imported separately using the
+right packages.
 */
 module ezdb;
 
 public
 {
     import ezdb.repository;
+    import ezdb.entity : primaryKey;
+    import ezdb.driver.common;
 }
